57f84cc7b14f49010db3f6b7fdbc6b442f5c9510
Simhadri Mohith <68764146+Mohith005@users.noreply.github.com>
2026-09-01 23:16:35 +0530

    Add files via upload

---
+++ 01_Basics/output.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // for std::cout
+
+int main()
+{
+    int x{5};
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "X is equal to: " << x << '\n';
+    std::cout << "Yup" << "\n";
+    std::cout << "And that's all folks!\n";
+
+    return 0;
+}
